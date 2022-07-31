59a1405914b99759ba04a7015f0b56cc07d8cef5
Jasir K <62549297+jasir721@users.noreply.github.com>
2022-08-01 01:43:05 +0530

    Delete queries.sql

--- queries.sql
+++
@@ -1,4 +0,0 @@
---trainlist from src to dest
-select * from trainlist('Kanpur Central Railway Station (CNB)', 'Allahabad Junction Railway Station (ALD)')
-
---show functionality of a user cancelling and a waiting list getting confirmed
